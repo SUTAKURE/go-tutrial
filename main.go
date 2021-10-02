77cb6df920c264c96ebdc253475a1cdf24424a05
Shinnosuke Hosaka <shinnosuke.h@latona.io>
2021-10-03 08:40:40 +0900

    delete newline

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 func main() {
-	t := time.Now().UnixNano()
-	rand.Seed(t)
-	random := rand.Intn(6)
-
-	switch random {
-	case 1:
-		fmt.Println("凶")
-	case 2, 3:
-		fmt.Println("吉")
-	case 4, 5:
-		fmt.Println("中吉")
-	case 6:
-		fmt.Println("大吉")
+	var sum int
+	sum = 5 + 6 + 3
+	avg := sum / 3
+	if avg > 4 {
+		println("good")
 	}
 }
