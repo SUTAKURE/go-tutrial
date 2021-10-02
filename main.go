620693a29797922ca759bef920318c8b94330b12
Shinnosuke Hosaka <shinnosuke.h@latona.io>
2021-10-02 22:53:38 +0900

    add switch

--- main.go
+++ main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("price")
+
+	num := 1
+	switch num {
+	case 1:
+		fmt.Printf("I \n")
+		fallthrough
+	case 2:
+		fmt.Printf("am \n")
+		fallthrough
+	case 3:
+		fmt.Printf("yyh-gl \n")
+	}
 }
