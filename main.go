4af9199fd8856306343c3c8918a847781de4212e
Shinnosuke Hosaka <shinnosuke.h@latona.io>
2021-10-03 10:17:08 +0900

    created odd-even-function

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-type Game struct {
-	Name     string
-	Point    int
-	PointSum int
+func main() {
+	for i := 1; i <= 100; i++ {
+		hantei(i)
+	}
 }
 
-func main() {
-
-	// deal
+func hantei(x int) {
+	print(x)
+	if x%2 == 0 {
+		println("-偶数")
+	} else {
+		println("-奇数")
+	}
 }
