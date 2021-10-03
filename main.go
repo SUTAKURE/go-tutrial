b0a852a4e5e7c7cc78cbf6b4671ac836679a986b
Shinnosuke Hosaka <shinnosuke.h@latona.io>
2021-10-03 11:02:22 +0900

    update-receiver

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
+type MyInt int
+
+func (n *MyInt) Inc() { *n++ }
 func main() {
-	n, m := 10, 20
-	swap2(&n, &m)
-	println(n, m)
-
-}
-
-func swap2(n, m *int) {
-	*n, *m = *m, *n
+	var n MyInt
+	println(n)
+	n.Inc()
+	println(n)
 }
