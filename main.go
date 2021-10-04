4bd6a9b977437e4efea123f3dba89ccfdd3e2218
Shinnosuke Hosaka <shinnosuke.h@latona.io>
2021-10-04 14:18:27 +0900

    create-main

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "go-tutrial/foobar"
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
 
+// 設定される変数のポインタを取得
+var msg = flag.String("msg", "デフォルト値", "説明")
+var n int
+
+func init() {
+	// ポインタを指定して設定を予約
+	flag.IntVar(&n, "n", 1, "回数")
+}
 func main() {
-	foobar.Foo()
-	foobar.Bar()
+	// ここで実際に設定される
+	flag.Parse()
+	fmt.Println(strings.Repeat(*msg, n))
 }
